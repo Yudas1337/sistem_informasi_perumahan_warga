423feec0a55c756311c141394989342e158f8491
Yudas <yudasmalabi@gmail.com>
2022-06-23 00:37:19 +0700

    Update navbar.blade.php

--- resources/views/dashboard/layouts/navbar.blade.php
+++ resources/views/dashboard/layouts/navbar.blade.php
@@ -41,12 +41,7 @@
                 @endif
             </a>
             <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdown-user"><a class="dropdown-item"
-                    href="{{ route('user.profile') }}"><i class="me-50" data-feather="user"></i> Profile</a><a
-                    class="dropdown-item" href="app-email.html"><i class="me-50" data-feather="mail"></i>
-                    Inbox</a><a class="dropdown-item" href="app-todo.html"><i class="me-50"
-                        data-feather="check-square"></i>
-                    Task</a><a class="dropdown-item" href="app-chat.html"><i class="me-50"
-                        data-feather="message-square"></i> Chats</a>
+                    href="{{ route('user.profile') }}"><i class="me-50" data-feather="user"></i> Profile</a>
                 <div class="dropdown-divider"></div>
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
